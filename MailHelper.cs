9240d619d609160d8217e13ac1d04e11579a4ecd
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2022-07-09 10:25:14 +0700

    up

---
+++ MailHelper.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.CodeParser;
+using DevExpress.DataProcessing;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecycledManagement.Common
+{
+    public class MailHelper
+    {
+        public MailHelper()
+        {
+            isBodyHtml = true;
+            DeliveryMethod = SmtpDeliveryMethod.Network;
+        }
+        public MailAddress FromMailAddress { get; set; }
+        public string Password { get; set; }
+        public string  Host { get; set; }
+        public string Port { get; set; }
+        public bool EnalbleSsl { get; set; }
+        public bool UseDefaultCredentials { get; set; }
+        public string  Subject { get; set; }
+        public bool isBodyHtml { get; set; }
+        public string Body { get; set; }
+        public SmtpDeliveryMethod DeliveryMethod { get; set; }
+        public string ToMailAddress { get; set; }
+        public string CCMailAddress { get; set; }
+
+        MailMessage message;
+        SmtpClient smtp;
+        
+        public bool SendEmail()
+        {
+            try
+            {
+                message = new MailMessage();
+                smtp = new SmtpClient();
+                message.From = FromMailAddress;
+                message.To.Add(ToMailAddress);
+                message.CC.Add(CCMailAddress);
+                message.Subject = Subject;
+                message.IsBodyHtml = isBodyHtml;
+                message.Body = Body;
+                smtp.Port = Convert.ToInt32(Port);
+                smtp.TargetName = "STARTTLS/smtp.office365.com";
+                smtp.Host = Host;
+                smtp.EnableSsl = EnalbleSsl;
+                smtp.UseDefaultCredentials = UseDefaultCredentials;
+                smtp.Credentials = new NetworkCredential(FromMailAddress.Address, Password);
+                smtp.DeliveryMethod = DeliveryMethod;
+                smtp.Send(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                XtraMessageBox.Show("Lỗi: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+        }
+    }
+}
---
+++ SendMailUseSmtpServer/SendMailUseSmtpServer/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SendMailUseSmtpServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MailHelper mailHelper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            #region khởi tạo đối tượng sendEmail
+            mailHelper = new MailHelper()
+            {
+                FromMailAddress = new MailAddress("fvn-itsupport@framas.com"),
+                Host = "smtp.office365.com",
+                Password = "fvnIT23!",
+                Port = "587",
+
+                ToMailAddress = "cong.nguyen@framas.com",
+                CCMailAddress = "ndcong08cddv02@gmail.com",
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                EnalbleSsl = true,
+                isBodyHtml = false,
+                UseDefaultCredentials = false,
+                Subject = "Test",
+                Body = null
+            };
+            #endregion
+
+            #region kiem tra gui email bao cân lố
+            //foreach (var item in productMixListBOM)
+            //{
+            //    if (item.MaterialCode.Contains("RCP") || item.MaterialCode.Contains("RMB") || item.MaterialCode.Contains("REX") || item.MaterialCode.Contains("RAD"))
+            //    {
+            //        if (Convert.ToDouble(item.ActualUsage) > (Convert.ToDouble(item.Total) + rangeColor))
+            //        {
+            //            mailHelper.Body = $"{mailHelper.Body}The material <{item.MaterialCode} - {item.MaterialName}> was used with the ratio <{item.ActualUsage}/{item.Total} > on <{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}> comparing to the planned ratio <{item.Quantity}>{Environment.NewLine}";
+            //        }
+            //    }
+            //}
+
+            //if (!string.IsNullOrEmpty(mailHelper.Body))
+            //{
+            //    mailHelper.Subject = "[Notification] - Recycled Management System";
+            //    mailHelper.Body = $"The exceed allowed ratio was found in the item <{orderInfo.ItemCode} - {orderInfo.ItemName}>, color <{orderInfo.ColorCode} - {orderInfo.ColorName}>{Environment.NewLine}{mailHelper.Body}_________________{Environment.NewLine}NOTE: This email was sent from a notification-only email address, please do not reply to this email.";
+            //    Debug.WriteLine(mailHelper.Body);
+
+            //    Task<bool> task = new Task<bool>(() => mailHelper.SendEmail());
+            //    task.Start();
+            //    //task.ContinueWith(t => XtraMessageBox.Show(t.Result.ToString()));
+            //}
+            mailHelper.Subject = $"[{DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy")}]";
+            mailHelper.Body = $"test mail server";
+
+            Task<bool> task = new Task<bool>(() => mailHelper.SendEmail());
+            task.Start();
+            labStatus.Content = task.Result;
+            #endregion
+        }
+    }
+}
