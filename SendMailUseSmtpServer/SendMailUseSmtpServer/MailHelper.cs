a216e309ccb82ca8ab656f6ebc2f28d19ed034b8
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2025-11-13 10:46:12 +0700

    update

--- SendMailUseSmtpServer/SendMailUseSmtpServer/MailHelper.cs
+++ SendMailUseSmtpServer/SendMailUseSmtpServer/MailHelper.cs
@@ -19,11 +19,11 @@ namespace SendMailUseSmtpServer
         }
         public MailAddress FromMailAddress { get; set; }
         public string Password { get; set; }
-        public string  Host { get; set; }
+        public string Host { get; set; }
         public string Port { get; set; }
         public bool EnalbleSsl { get; set; }
         public bool UseDefaultCredentials { get; set; }
-        public string  Subject { get; set; }
+        public string Subject { get; set; }
         public bool isBodyHtml { get; set; }
         public string Body { get; set; }
         public SmtpDeliveryMethod DeliveryMethod { get; set; }
@@ -32,8 +32,8 @@ namespace SendMailUseSmtpServer
 
         MailMessage message;
         SmtpClient smtp;
-        
-        public bool SendEmail()
+
+        public string SendEmail()
         {
             try
             {
@@ -41,10 +41,11 @@ namespace SendMailUseSmtpServer
                 smtp = new SmtpClient();
                 message.From = FromMailAddress;
                 message.To.Add(ToMailAddress);
-                message.CC.Add(CCMailAddress);
+                //message.CC.Add(CCMailAddress);
                 message.Subject = Subject;
                 message.IsBodyHtml = isBodyHtml;
                 message.Body = Body;
+
                 smtp.Port = Convert.ToInt32(Port);
                 smtp.TargetName = "STARTTLS/smtp.office365.com";
                 smtp.Host = Host;
@@ -53,12 +54,12 @@ namespace SendMailUseSmtpServer
                 smtp.Credentials = new NetworkCredential(FromMailAddress.Address, Password);
                 smtp.DeliveryMethod = DeliveryMethod;
                 smtp.Send(message);
-                return true;
+                return "Send ok.";
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Lỗi: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return false;
+                return ex.Message;
             }
 
         }
--- SendMailUseSmtpServer/SendMailUseSmtpServer/MainWindow.xaml.cs
+++ SendMailUseSmtpServer/SendMailUseSmtpServer/MainWindow.xaml.cs
@@ -35,46 +35,28 @@ namespace SendMailUseSmtpServer
                 Password = "fvnIT23!",
                 Port = "587",
 
-                ToMailAddress = "cong.nguyen@framas.com",
-                CCMailAddress = "ndcong08cddv02@gmail.com",
+                //ToMailAddress = "cong.nguyen@framas.com",
+                //CCMailAddress = "ndcong08cddv02@gmail.com",
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 EnalbleSsl = true,
                 isBodyHtml = false,
                 UseDefaultCredentials = false,
                 Subject = "Test",
-                Body = null
+                Body = $"Truck pack{Environment.NewLine}EmployeeId: 7387{Environment.NewLine} Request out."
             };
             #endregion
+        }
 
-            #region kiem tra gui email bao cân lố
-            //foreach (var item in productMixListBOM)
-            //{
-            //    if (item.MaterialCode.Contains("RCP") || item.MaterialCode.Contains("RMB") || item.MaterialCode.Contains("REX") || item.MaterialCode.Contains("RAD"))
-            //    {
-            //        if (Convert.ToDouble(item.ActualUsage) > (Convert.ToDouble(item.Total) + rangeColor))
-            //        {
-            //            mailHelper.Body = $"{mailHelper.Body}The material <{item.MaterialCode} - {item.MaterialName}> was used with the ratio <{item.ActualUsage}/{item.Total} > on <{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}> comparing to the planned ratio <{item.Quantity}>{Environment.NewLine}";
-            //        }
-            //    }
-            //}
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            mailHelper.ToMailAddress = txtToEmail.Text;
+            mailHelper.CCMailAddress = txtCCEmail.Text;
+            mailHelper.Subject = $"[{DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy")}] {txtSubject.Template}";
+            mailHelper.Body = $"{txtBody.Text}";
 
-            //if (!string.IsNullOrEmpty(mailHelper.Body))
-            //{
-            //    mailHelper.Subject = "[Notification] - Recycled Management System";
-            //    mailHelper.Body = $"The exceed allowed ratio was found in the item <{orderInfo.ItemCode} - {orderInfo.ItemName}>, color <{orderInfo.ColorCode} - {orderInfo.ColorName}>{Environment.NewLine}{mailHelper.Body}_________________{Environment.NewLine}NOTE: This email was sent from a notification-only email address, please do not reply to this email.";
-            //    Debug.WriteLine(mailHelper.Body);
-
-            //    Task<bool> task = new Task<bool>(() => mailHelper.SendEmail());
-            //    task.Start();
-            //    //task.ContinueWith(t => XtraMessageBox.Show(t.Result.ToString()));
-            //}
-            mailHelper.Subject = $"[{DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy")}]";
-            mailHelper.Body = $"test mail server";
-
-            Task<bool> task = new Task<bool>(() => mailHelper.SendEmail());
+            Task<string> task = new Task<string>(() => mailHelper.SendEmail());
             task.Start();
             labStatus.Content = task.Result;
-            #endregion
         }
     }
 }
